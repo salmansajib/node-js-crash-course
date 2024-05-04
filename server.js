e6ab2803517afd8ef1baba78ff95574ea049cda0
Salman-sajib <sfsajib@gmail.com>
2024-05-04 15:20:01 +0600

    created the server port in env file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   // res.write('Hello World');
   // res.setHeader('Content-type', 'text/html');
   // res.statusCode = 404;
+  console.log(req.url);
+  console.log(req.method);
 
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('<h1>Hello World</h1>');
