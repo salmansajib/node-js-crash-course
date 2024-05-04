4ab57afbdb69f229da0166f92f8685bfcad991cc
Salman-sajib <sfsajib@gmail.com>
2024-05-04 11:24:53 +0600

    export another function using commonjs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const generateRandomNumber = require('./utils');
+const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
 
 console.log(`Random Number: ${generateRandomNumber()}`);
+
+console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(0)}`);
